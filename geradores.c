aae7b8977585ab4a2ac1df676d1f26dcd0a4318e
Julia Thais <julia.gomes@ee.ufcg.edu.br>
2023-02-02 20:22:46 -0300

    [Refactor] Acrescenta a variável 'temperatura_atual' na função 'GeradorTemperatura()'

--- geradores.c
+++ geradores.c
@@ -26,10 +26,23 @@ void GeradorPresenca() {
 
 void GeradorTemperatura() {
 
-    int i = 0, MaxArray = 31, mediaTemperatura = 25, maxTemperatura = 26, minTemperatura = 24;
+    int i = 0, MaxArray = 30, temperatura_atual = 0, mediaTemperatura = 25, maxTemperatura = 26, minTemperatura = 24;
     FILE* arqDadosTemperatura;
     
     while (1) {
+      
+        for (i = 0; i < MaxArray; i++) {
+            if (i >= 0) {
+                temperatura_atual = maxTemperatura;
+            }
+            if (i >= MaxArray/3 && i < 2/3*MaxArray) {
+                temperatura_atual = mediaTemperatura;
+            }
+            else if (i >= 2/3*MaxArray && i < MaxArray) {
+                temperatura_atual = minTemperatura;
+            }
+        }
+
         arqDadosTemperatura = fopen("DadosTemperatura.txt", "w");
 
         if (arqDadosTemperatura == NULL) {
@@ -37,24 +50,8 @@ void GeradorTemperatura() {
             exit(1);
         }
 
-        for (i = 0; i < MaxArray; i++) {
-            if (i < MaxArray / 3) {
-                fprintf(arqDadosTemperatura, "%d", mediaTemperatura);
-            }
-            else if (i >= MaxArray / 3 && i < 2 / 3 * MaxArray) {
-                fprintf(arqDadosTemperatura, "%d", maxTemperatura);
-            }
-            else if (i >= 2 / 3 * MaxArray && i < MaxArray) {
-                fprintf(arqDadosTemperatura, "%d", minTemperatura);
-            }
-        }
-
-        i = 0;
-
+        fprintf(arqDadosTemperatura, "%d ", temperatura_atual);
         fclose(arqDadosTemperatura);
-
-        printf(" \nOs dados de temperatura do ambiente foram gerados");
-        vTaskDelay(1000000);
     }
 
 }
